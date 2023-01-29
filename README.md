2c7ee507736323aadf8a00219c3272269e66a510
James Gordo <hello@jamesgordo.com>
2023-01-29 11:57:00 +0800

    Added Eslint & Prettier

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # express-bp
+
 A Minimal ExpressJS Boilerplate.
 
 ## PM2 Support
+
 To start using `pm2`, run the following command:
+
 ```
 pm2 start ecosystem.config.js
 ```
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,12 @@
 module.exports = {
-  apps : [{
-    name   : "express-app",
-    script : "./index.js",
-    watch: process.env.NODE_ENV !== 'production',
-    ignore_watch: ['[\/\\]\./', 'node_modules'],
-    error_file: './logs/error.log',
-    log_file: './logs/access.log',
-  }]
+  apps: [
+    {
+      name: 'express-app',
+      script: './index.js',
+      watch: process.env.NODE_ENV !== 'production',
+      ignore_watch: ['[/\\]./', 'node_modules'],
+      error_file: './logs/error.log',
+      log_file: './logs/access.log',
+    },
+  ],
 }
